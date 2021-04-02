d3a10aac60e73dbfcfe8d55143b8ec9ea60b8728
Venkat <81518011+padavalavenkatesh@users.noreply.github.com>
2021-04-02 11:28:58 +0530

    ok

--- SpeedLimit/script.js
+++ SpeedLimit/script.js
@@ -2,13 +2,14 @@ var speed;
 var maxspeed = 50;
 var Penality;
 function sp(speed) {
-  if (speed < maxspeed) {
+  if (speed > maxspeed) {
     console.log(`violated the speed limit`);
-    if (speed > 50) {
-      penality = speed * 100;
-      console.log(`fine to be paid: ${penality}`);
-    }
+    penality = speed * 100;
+    console.log(`fine to be paid: ${penality}`);
+  } else {
+    console.log("Vehicle is in good speed");
   }
 }
-var x = sp(10);
-var y = sp(100);
+
+sp(10);
+sp(100);
